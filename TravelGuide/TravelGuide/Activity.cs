45b247548108becbcf043b63bd702ef08375720d
Pavel <ppurlantov@yahoo.com>
2016-06-18 22:30:49 +0300

    Initial commit
    Team Superman

---
+++ TravelGuide/TravelGuide/Activity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace superman
+{
+    public enum Activity
+    {
+        Culture = 0,
+        Nature = 1,
+        Skiing = 2,
+        Beach = 3,
+        Mountain = 4,
+        ExtremeSports = 5
+
+    }
+}
---
+++ TravelGuide/TravelGuide/Continent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TourGuide
+{
+    public class Continent : Guide
+    {
+        public int MyProperty { get; set; }
+    }
+}

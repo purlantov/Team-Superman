02bf2ee70baa4cc9736b51fa9cf4d7757c6961c3
Pavel <ppurlantov@yahoo.com>
2016-07-01 06:16:55 +0300

    mini gain

--- TravelGuide_version1.0/TravelGuide/Engine.cs
+++ TravelGuide_version1.0/TravelGuide/Engine.cs
@@ -9,6 +9,7 @@ namespace TravelGuide
     {
         public static void Run ()
         {
+            #region Test reding counties from file
             //Tetsting reading country from file
             Country country = new Country();
             var listOfCountries  = country.GetCountry();
@@ -17,10 +18,22 @@ namespace TravelGuide
             {
                 Console.WriteLine(item);
             }
+            Console.WriteLine(new string('=',15));
 
+            var list = listOfCountries.Select(x => x.Name == "ASIA");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item); //returns TRUE or FALSE
+            }
+            #endregion
+
+            #region Test Windows Forms Show
             //Diplay -- Print Dialog Form
             //GuideForm form = new GuideForm();
             //form.ShowDialog();
+            #endregion
+
+
         }
     }
 }

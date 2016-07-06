d702da22a1b641f2fe97fe585dff654cdd8a74b8
shopov91 <shopov91@users.noreply.github.com>
2016-07-06 18:55:02 +0300

    Adding 5  Housing classes.

---
+++ BudgetTourist/BudgetTourist/Housing/Hostel.cs
@@ -0,0 +1,19 @@
+﻿namespace BudgetTourist.Housing
+{
+    using Contracts;
+
+    public class Hostel : Housing, IHostel
+    {
+        public Hostel(string name, int numberOfPeople, string location, decimal price, string parkingSpace, int numberOfBeds)
+            : base(name, location, price, parkingSpace)
+        {
+            this.Name = name;
+            this.Location = location;
+            this.Price = price;
+            this.ParkingSpace = parkingSpace;
+            this.NumberOfBeds = numberOfBeds;
+        }
+
+        public int NumberOfBeds { get; private set; }
+    }
+}
---
+++ BudgetTourist/BudgetTourist/Housing/Housing.cs
@@ -0,0 +1,50 @@
+﻿namespace BudgetTourist.Housing
+{
+    using Contracts;
+    public abstract class Housing : IHousing   // all housing types will derive from this abstract class Housing
+    {
+        #region Fields
+        private string name;
+
+        private string location;
+
+        private decimal price;
+
+        private string parkingSpace;
+        #endregion
+
+        #region Constuctors
+        public Housing(string name, string location, decimal price, string parkingSpace)
+        {
+            this.Name = name;
+            this.Location = location;
+            this.Price = price;
+            this.ParkingSpace = parkingSpace;
+        }
+        #endregion
+
+        #region Properties
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public string Location
+        {
+            get { return this.location; }
+            set { this.location = value; }
+        }
+
+        public decimal Price
+        {
+            get { return this.price; }
+            set { this.price = value; }
+        }
+        #endregion
+        public string ParkingSpace
+        {
+            get { return this.parkingSpace; }
+            set { this.parkingSpace = value; }
+        }
+    }
+}

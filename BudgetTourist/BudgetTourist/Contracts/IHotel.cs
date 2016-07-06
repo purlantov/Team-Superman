409d54884027418f5f255f70a74e7e5971de9628
shopov91 <shopov91@users.noreply.github.com>
2016-07-06 19:06:47 +0300

    Update IHotel.cs

--- BudgetTourist/BudgetTourist/Contracts/IHotel.cs
+++ BudgetTourist/BudgetTourist/Contracts/IHotel.cs
@@ -2,7 +2,7 @@
 {
     public interface IHotel
     {
-        double NumberOfStars { get; }  //??? maybe well have hotels with like 4.5 stars... i don't know?!!?!? double/int/string??
+        double NumberOfStars { get; }  //??? maybe we'll have hotels with like 4.5 stars... i don't know?!!?!? double/int/string??
         int NumberOfRooms { get; }
         string Breakfast { get; }
 

5f5136293621d80d8519e3201508367c4215591e
Natali <aracna2k@yahoo.com>
2016-07-07 22:16:06 +0300

    adding events part

---
+++ TeamWork_EventsPart/TeamWork_EventsPart/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamWork_EventsPart
+{
+    public class Event : ActivitiesAbstr
+    {
+        public Event(string name, double price, string location, DateTime startTime, EnumEventType type) : base(name, price, location, startTime)
+        {
+            this.Type = type;
+        }
+
+        public EnumEventType Type { get; }
+    }
+}
---
+++ TeamWork_EventsPart/TeamWork_EventsPart/IActivities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamWork_EventsPart
+{
+    public interface IActivities
+    {
+       
+        double Pay(double money);
+        
+
+    }
+}

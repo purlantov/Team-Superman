4e1c2aaf782834d811872ef79cdecce75c21ecc5
JesusLoovesYou <ivanangelov87@gmail.com>
2016-07-09 23:30:26 +0300

    Transport

---
+++ BudgetTourist/BudgetTourist/Transport/Transport.cs
@@ -0,0 +1,70 @@
+﻿namespace TeamsSuperMan
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class Transport
+    {
+        private CarList carList;
+        private CityTransportCardsList cityCartList;
+        private List<ICartable> userCarts;
+
+        public Transport()
+        {
+            carList = new CarList();
+            cityCartList = new CityTransportCardsList();
+            userCarts = new List<ICartable>();
+        }
+
+        //Rent a Car methods
+        public List<IVehicles> GetCars()
+        {
+            return carList.GetCars();
+        }
+
+        public decimal GetPrceOfReservedCar(IVehicles car, DateTime start, DateTime end)
+        {
+            return carList.GetPrceOfReservedCar(car, start, end);
+        }
+
+        // City Transport Carts methods
+        public List<ICartable> GetCarts()
+        {
+            return cityCartList.GetCarts();
+        }
+
+        public List<ICartable> GetUserCarts()
+        {
+            return this.userCarts;
+        }
+
+        /// <summary>
+        /// Add carts in userCarts
+        /// </summary>
+        /// <param name="cart">Cart</param>
+        /// <param name="countOfCarts">Count of carts thath user wont</param>
+        public void AddCart(ICartable cart, int countOfCarts = 1)
+        {
+            for (int i = 0; i < countOfCarts; i++)
+            {
+                userCarts.Add(cart);                
+            }
+        }
+
+        public void DeleteCart(ICartable cart)
+        {
+            userCarts.Remove(cart);
+        }
+
+        public void DeleteAllCarts()
+        {
+            userCarts.Clear();
+        }
+
+        public decimal GetPriceOfUserCarts()
+        {
+            return userCarts.Sum(x => x.Price);
+        }
+    }
+}

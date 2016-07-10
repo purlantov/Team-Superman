ec6c8a7e86fa0e890d07214e155bbb34d24a56b0
Emo <emo960427@gmail.com>
2016-07-10 15:34:19 +0300

    -recommit

---
+++ BudgetTourist/BudgetTourist/Food/MenuMaker.cs
@@ -0,0 +1,26 @@
+﻿namespace ProjectFood
+{
+    using Contracts;
+    using Enums;
+    using Models;
+    
+    public class MenuMaker
+    {
+        public IFood CreateApetizer(string dishName, DishType dishType,double portionSize,double price)
+        {
+            return new Appetizer(dishName,dishType,portionSize,price);
+        }
+        public IFood CreateMainDish(string dishName, DishType dishType, double portionSize, double price)
+        {
+            return new MainDish(dishName, dishType, portionSize, price);
+        }
+        public IFood CreateDessert(string dishName, DishType dishType, double portionSize, double price)
+        {
+            return new Dessert(dishName, dishType, portionSize, price);
+        }
+        public IDrink CreateDrink(string dishName, DrinkType drinkType, double portionSize, double price)
+        {
+            return new Drink(dishName, drinkType, portionSize, price);
+        }
+    }
+}

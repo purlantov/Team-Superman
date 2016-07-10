c41b954f43f69da05296accc7eb2fd7aee346dea
Emo <emo960427@gmail.com>
2016-07-10 20:36:11 +0300

    Order finished

---
+++ BudgetTourist/BudgetTourist/Models/FoodModels/FoodOrder.cs
@@ -0,0 +1,29 @@
+﻿using BudgetTourist.Contracts.FoodContracts;
+using System.Collections.Generic;
+
+
+namespace BudgetTourist.Models.FoodModels
+{
+    public class FoodOrder
+    {
+        private double totalPrice = 0;
+        
+
+        IList<IConsumable> orderList { get; set; }
+        public double TotalPrice
+        {
+            get
+            {
+                return this.totalPrice;
+            }
+            private set
+            {
+                foreach (var dish in orderList)
+                {
+                    totalPrice += dish.Price;
+                }
+            }
+        }
+        
+    }
+}
